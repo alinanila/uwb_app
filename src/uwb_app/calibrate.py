cb9807d19503543f37b35ac4e514f7dba9dbc765
alinanila <ar3822@ic.ac.uk>
2026-04-01 22:37:26 +0100

    possible implementation of 3d anchor calibration
    added solve trilateration function to calibrate a fourth anchor elevated above the other 3 if manage to make 3d localisation work

--- src/uwb_app/calibrate.py
+++ src/uwb_app/calibrate.py
@@ -152,22 +152,53 @@ def collect_distances(
     return avgs
 
 
-def solve_bilateration(D_x: float, dist_A: float, dist_D: float) -> tuple[float, float]:
+def solve_bilateration(
+    D_x: float, 
+    dist_A: float, 
+    dist_D: float
+) -> tuple[float, float]:
     """
-    assuming A = (0,0) and D = (D_x,0), solve for position of anchor B or C with tag held over it
-    making the assumption that anchors B and C are 'above' the x-axis by taking positive sqrt
+    solve for x,y (anchor C specifically)
+    assuming A = (0,0) and D = (D_x,0) to define the x-axis
+    making the assumption that anchors B and C are above the x-axis by taking positive sqrt
     """
     x = (D_x**2 + dist_A**2 - dist_D**2) / (2.0 * D_x)
     y_sq = dist_A**2 - x**2
     if y_sq < 0:
         y_sq = 0.0
     y = math.sqrt(y_sq)
+    z = 0.0
+    # return x, y, z, need to add another float to tuple return in definition
     return x, y
+
+
+def solve_trilateration(
+    D_x: float,
+    C_x: float,
+    C_y: float,
+    dist_A: float,
+    dist_D: float,
+    dist_C: float,
+) -> tuple[float, float, float]:
+    """
+    solve for x,y,z (anchor B specifically)
+    assuming A = (0,0,0), D = (D_x,0,0) and C = (C_x,C_y,0) to define the xy-plane
+    making the assumption that anchor B is above the xy-plane by taking positive sqrt
+    requires anchor B to not be coplanar with other three
+    """
+    x = (D_x**2 + dist_A**2 - dist_D**2) / (2.0 * D_x)
+    y = (C_x**2 + C_y**2 - 2.0 * C_x * x + dist_A**2 - dist_C**2) / (2.0 * C_y)
+    z_sq = dist_A**2 - x**2 - y**2
+    if z_sq < 0:
+        z_sq = 0.0
+    z = math.sqrt(z_sq)
+    return x, y, z
 
 
 def update_layout(
     config_path: Path,
     anchors_out: Dict[str, tuple[float, float]],
+    # anchors_out: Dict [str, tuple[float, float, float]],
 ) -> None:
     """
     update anchor layout in uwb_localizer.yaml with calibrated layout
@@ -190,6 +221,9 @@ def update_layout(
     # set new anchor positions
     for source_id, (x, y) in anchors_out.items():
         anchors[source_id] = [float(x), float(y)]
+
+    # for source_id, (x, y, z) in anchors_out.items():
+    #     anchors[source_id] = [float(x), float(y), float(z)]
 
     with target_path.open("w", encoding="utf-8") as f:
         yaml.safe_dump(data, f, sort_keys=False)
@@ -242,6 +276,7 @@ def main() -> None:
     )
     print("distances at anchor A:", dists_Apos)
     A = (0.0, 0.0)
+    # A = (0.0, 0.0, 0.0)
 
     # D (dist, 0) (defining the x-axis)
     input("place tag at anchor D and press enter")
@@ -256,6 +291,7 @@ def main() -> None:
             "no distance from anchor A when tag at anchor D; check IDs/config"
         )
     D = (dist_AD, 0.0)
+    # D = (dist_AD, 0.0, 0.0)
 
     # solve for C from A and D
     input("place tag at anchor C and press enter")
@@ -277,17 +313,20 @@ def main() -> None:
     input("place tag at anchor B and press enter")
     dists_Bpos = collect_distances(
         args.hub_endpoint, args.topic, args.avg_seconds, args.peer_id, {"ANCHOR:A", "ANCHOR:D"}
+        # args.hub_endpoint, args.topic, args.avg_seconds, args.peer_id, {"ANCHOR:A", "ANCHOR:D", "ANCHOR:C"}
     )
     print("distances at anchor B:", dists_Bpos)
     # measure from A and D
     dist_AB = dists_Bpos.get("ANCHOR:A")
     dist_DB = dists_Bpos.get("ANCHOR:D")
-    if dist_AB is None or dist_DB is None:
+    dist_CB = dists_Bpos.get("ANCHOR:C")
+    if dist_AB is None or dist_DB is None or dist_CB is None:
         raise RuntimeError(
-            "missing distance from anchor A and/or D; check IDs/config"
+            "missing distance from anchor A and/or D and/or C; check IDs/config"
         )
     
     B = solve_bilateration(D_x=D[0], dist_A=dist_AB, dist_D=dist_DB)
+    # B = solve_trilateration(D_x=D[0], C_x=C[0], C_y=C[1], dist_A=dist_AB, dist_D=dist_DB, dist_C=dist_CB)
 
     print("\n calibrated anchor positions:")
     print(f"ANCHOR:A = {A}")
